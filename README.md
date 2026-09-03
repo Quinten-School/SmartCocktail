2591dc0be1aa54135fd2c1ca8f45936ee677333f
QuintenVanDommelen <quinten.vandommelen@telenet.be>
2026-09-03 19:00:05 +0200

    initial commit, website = template for later

---
+++ README.md
@@ -0,0 +1,16 @@
+This is a template website for my STEM-project.
+
+index.html --> information page
+login.html --> page for login
+
+dashboard.html
+settings.html
+
+
+css/ style.css
+imgs/
+includes/
+    footer.html
+    footer.js
+    header.html
+    header.js
---
+++ public/includes/header.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/public/includes/header.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("header-placeholder").innerHTML = data;
+
+      // Titel uit <body data-title="...">
+      const title = document.body.dataset.title;
+      const titleElement = document.getElementById("page-title");
+      if (title && titleElement) {
+        titleElement.textContent = title;
+      }
+    });
+});
