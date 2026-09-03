97ec9ac629007ca27beeccd2859b4fa605ade11f
Quinten-School <quinten.vandommelen@telenet.be>
2026-09-03 19:10:53 +0200

    Fix fetch path for header.html

--- public/includes/header.js
+++ public/includes/header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("/public/includes/header.html")
+  fetch("public/includes/header.html")
     .then(response => response.text())
     .then(data => {
       document.getElementById("header-placeholder").innerHTML = data;
